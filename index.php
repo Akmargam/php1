100a2acdfda3079a02227ac005f686e96a9bf600
Arun <aruway333@gmail.com>
2024-02-19 14:39:08 -0500

    basics

--- index.php
+++ index.php
@@ -7,9 +7,22 @@
 </head>
 <body>
     <?php
-        echo "Hello World";
-        echo "What's up?";
+        $completed=true;
+        $score=99;
+        $price=0.99;
+        $greet='hi';
 
+        echo $completed . '<br/>';
+        var_dump($greet).'<br/>';
+        $companies=[1,2,3,4,5] ;
+        print_r($companies);
+
+        // arrays
+
+        $progLang=['PHP','Python','Java'];
+        echo $progLang[2];
+
+        //operators
     ?>
 </body>
 </html>
